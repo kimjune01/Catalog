50c6da742342a1aaa0f72de09aa575850fa518b6
June <kimjune01@gmail.com>
2024-10-08 00:10:14 -0700

    Initial Commit

---
+++ SwiftyCatalog/SwiftyCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyCatalogApp.swift
+//  SwiftyCatalog
+//
+//  Created by June Kim on 2024-10-08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
